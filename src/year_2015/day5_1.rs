ddfd7a2ef476eda36ddb67195adaa152be1195b1
Kevin Vinther <kevinjoshua.kjv@gmail.com>
2021-11-19 21:46:21 +0100

    Add correct solution for d5p1

--- src/year_2015/day5_1.rs
+++ src/year_2015/day5_1.rs
@@ -3,38 +3,40 @@
 
 // TODO: this is a very slow solution, but it works
 pub fn solve(input: &str) -> usize {
-    let vowels = vec!["a", "e", "i", "o", "u"];
+    let vowels: &str = "aeiou";
     let double_letters = vec!["aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", "nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"];
     let not_allowed = vec!["ab", "cd", "pq", "xy"];
-    let mut count = 0;
-    let mut nice_meter; 
+    let mut count: usize = 0;
+    let mut nice_meter;
     let mut vowel_count;
+
+    // TODO: I'm sure this can be done more efficiently
     for word in input.split_whitespace() {
-        nice_meter = 0; 
+        nice_meter = 0;
         vowel_count = 0;
-        'a: for letter in &vowels {
-            if word.contains(letter) {
+        for letter in word.chars() {
+            if vowels.contains(letter) {
                 vowel_count += 1;
-                if vowel_count >= 3 {
-                    nice_meter += 1;
-                    break 'a;
-                }
             }
         }
-        'b: for letter in &double_letters {
-            if word.contains(letter) {
-                nice_meter += 1; 
-                break 'b;
+        'a: for double_letter in double_letters.iter() {
+            if word.contains(double_letter) {
+                nice_meter += 1;
+                break 'a;
             }
         }
-        'c: for letter in &not_allowed {
-            if word.contains(letter) {
-                nice_meter = 0; 
-                break 'c;
+        if vowel_count >= 3 {
+            nice_meter += 1;
+        }
+
+        for pair in &not_allowed {
+            if word.contains(pair) {
+                nice_meter = 0;
+                break;
             }
         }
         if nice_meter >= 2 {
-            count += 1; 
+            count += 1;
         }
     }
     count
